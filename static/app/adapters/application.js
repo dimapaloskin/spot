0dd746885ac210acee8493d69e6f436395fdea4f
Dmitry <dima@paloskin.me>
2016-03-14 14:08:15 +0300

    [feat] arrow fns

--- static/app/adapters/application.js
+++ static/app/adapters/application.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
   namespace: 'api/v1',
   host: 'https://localhost:3000',
-  ajax: function(url, method, hash) {
+  ajax: (url, method, hash) => {
     hash.crossDomain = true;
     hash.xhrFields = { withCredentials: true };
     return this._super(url, method, hash);
